5308dac7e66f45ef7b395101e77cd3ade595dd57
sultan <aidaraliuulu@gmail.com>
2019-04-06 12:54:45 +0600

    Создал сущность Artist.js он умеет создовать artist и возврщать все artist.

---
+++ app/Artist.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const nanoid = require('nanoid');
+const config = require('../config');
+const multer = require('multer');
+const ArtistSchema = require('../modules/Artist');
+const router = express.Router();
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, config.uploadPath);
+    },
+    filename: (req, file, cb) => {
+        cb(null, nanoid() + path.extname(file.originalname));
+    }
+});
+
+const upload = multer({storage});
+
+router.get('/', (req, res) => {
+   ArtistSchema.find()
+       .then(result => res.send(result))
+       .catch(() => res.sendStatus(500))
+});
+
+router.post('/', (req, res) => {
+    const artistData = req.body;
+    artistData.id = nanoid();
+
+    if (req.file) {
+        artistData.image = req.file.filename;
+    }
+
+    const Artist = new ArtistSchema(artistData);
+
+    Artist.save()
+        .then(result => res.send(result))
+        .catch(error => res.status(400).send(error))
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,15 +2,16 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-// const artist = require('./app/Artist');
-
+const artists = require('./app/Artist');
 const app = express();
+
+app.use(express.json());
 app.use(cors());
 
-const port = 9050;
+const port = 9000;
 
 mongoose.connect('mongodb://localhost/lastFM', {useNewUrlParser: true}).then(() => {
-    // app.use('/artist', artist);
+    app.use('/artists', artists);
 
     app.listen(port, () => {
         console.log(`Server started on ${port} port!`);
